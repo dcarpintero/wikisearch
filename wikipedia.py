a41dfce337ddb2cad38628540740e6793384d353
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-09-27 17:25:12 +0200

    move search logic to wikipedia

--- wikipedia.py
+++ wikipedia.py
@@ -1,10 +1,25 @@
+from dotenv import load_dotenv
+import cohere
+import logging
+import os
+import weaviate
+
+
 class SearchEngine:
     """
     Class to perform keyword search, semantic search, and reranking
     """
-    def __init__(self, cohere, weaviate):
-        self.cohere = cohere
-        self.weaviate = weaviate
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s [%(levelname)s] %(message)s")
+        self.vars = self.__load_environment_vars()
+
+        self.cohere = self.__cohere_client(self.vars["COHERE_API_KEY"])
+        self.weaviate = self.__weaviate_client(
+            self.vars["WEAVIATE_API_KEY"],
+            self.vars["COHERE_API_KEY"],
+            self.vars["WEAVIATE_URL"],
+        )
         
     def by_keyword(self, query, lang='en', top_n=10):
         """
@@ -76,3 +91,46 @@ class SearchEngine:
             top_n = top_n,
         )
         return reranked_responses
+    
+    def __load_environment_vars(self):
+        """
+        Load environment variables from .env file
+        """
+        logging.info("Loading environment variables...")
+
+        load_dotenv()
+
+        cohere_api_key = os.getenv("COHERE_API_KEY")
+        weaviate_api_key = os.getenv("WEAVIATE_API_KEY")
+        weaviate_url = os.getenv("WEAVIATE_URL")
+
+        if not cohere_api_key:
+            raise EnvironmentError("COHERE_API_KEY environment variable not set.")
+        
+        if not weaviate_api_key:
+            raise EnvironmentError("WEAVIATE_API_KEY environment variable not set.")
+        
+        if not weaviate_url:
+            raise EnvironmentError("WEAVIATE_URL environment variable not set.")
+        
+        logging.info("Environment variables loaded.")
+        return {"COHERE_API_KEY": cohere_api_key, "WEAVIATE_API_KEY": weaviate_api_key, "WEAVIATE_URL": weaviate_url}
+        
+    def __cohere_client(self, cohere_api_key):
+        """
+        Initialize Cohere client
+        """
+        return cohere.Client(cohere_api_key)
+    
+    def __weaviate_client(self, weaviate_api_key, cohere_api_key, cohere_url):
+        """
+        Initialize Weaviate client
+        """
+        auth_config = weaviate.auth.AuthApiKey(api_key=weaviate_api_key)
+        return weaviate.Client(
+            url=cohere_url,
+            auth_client_secret=auth_config,
+            additional_headers={
+                "X-Cohere-Api-Key": cohere_api_key,
+            }
+        )
