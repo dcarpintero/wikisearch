363137c2dd3a4ff1e962fca78214fd67f42d24fe
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-09-29 15:51:23 +0200

    add Cohere Blog badge

--- app.py
+++ app.py
@@ -92,11 +92,12 @@ with st.sidebar.expander("🤖 COHERE-SETTINGS", expanded=True):
                             max_value=15, value=5, step=1)
 
 with st.sidebar.expander("🔧 WEAVIATE-SETTINGS", expanded=True):
-    st.toggle('Near Text Search', key="with_near_text",
+    st.toggle('Dense Retrieval', key="with_near_text",
               on_change=onchange_with_near_text)
-    st.toggle('BM25 Search', key="with_bm25", on_change=onchange_with_bm25)
-    st.toggle('Hybrid Search',  key="with_hybrid",
+    st.toggle('Keyword Search', key="with_bm25", on_change=onchange_with_bm25)
+    st.toggle('Hybrid Mode',  key="with_hybrid",
               on_change=onchange_with_hybrid)
+    st.info("ℹ️ Note that *Dense Retrieval* and *Hybrid* outperform *Keyword Search* on complex queries!")
 
 with st.expander("ℹ️ ABOUT-THIS-APP", expanded=False):
     st.write("""
@@ -104,8 +105,11 @@ with st.expander("ℹ️ ABOUT-THIS-APP", expanded=False):
              - Step 1: Pre-Search on *Weaviate* with Sparse Retrival (bm25), Dense Retrieval (neartext), or Hybrid Mode (bm25 + neartext).
              - Step 2: *Cohere Rank Model* re-organizes the Pre-Search by assigning a relevance score to each Pre-Search result given the query.
              - Step 3: *Cohere Generation Model* composes a response based on the ranked results.
-             - Ask on your preferred language, and experiment with the settings!
+             - Ask in your preferred language, and experiment with the settings!
              """)
+    
+with st.sidebar:
+    "[![LLMs for Search](https://img.shields.io/badge/LLMs%20for%20Search-gray)](https://txt.cohere.com/using-llms-for-search/)"
     
 def onclick_sample_query(query):
     st.write("onclick_sample_query: " + query)
@@ -114,7 +118,8 @@ def onclick_sample_query(query):
 # -----------------------------------------------------------------------------
 # Ask Wikipedia Section
 # -----------------------------------------------------------------------------
-query = st.text_input(label="Ask 'Wikipedia'", placeholder='Ask your question here, or select one from the examples below',  key="user_query_txt")
+st.subheader("Wikipedia Semantic Search")
+query = st.text_input(label="Ask 'Wikipedia'", placeholder='Ask your question here, or select one from the examples below',  key="user_query_txt", label_visibility="hidden")
 
 btn_printing = st.session_state.get("btn_printing", False)
 btn_nobel = st.session_state.get("btn_nobel", False)
@@ -144,9 +149,10 @@ if query:
     elif st.session_state.with_hybrid:
         data = query_hybrid(query, lang=lang_code, top_n=max_results)
     else:
-        st.info("ℹ️ Select your preferred Search Mode (Near Text, BM25 or Hybrid)!")
+        st.info("ℹ️ Select your preferred Search Mode (Dense Retrieval, Keyword Search, or Hybrid)!")
         st.stop()
 
+    st.divider()
     col1, col2, col3 = st.columns([1,1,1])
 
     with col1:
@@ -167,14 +173,14 @@ if query:
             st.divider()
 
     with col3:
-        st.subheader("📝 Generation")
+        st.subheader("📝 LLM Generation")
 
         with st.spinner("Deep Diving..."):
             r = query_llm(context=data_ranked, query=query, temperature=temperature, model=gen_model, lang=lang)
         st.success(r)
         
         with st.expander("📚 WIKIPEDIA-REFERENCES", expanded=True):
-            st.info("Some references might appear duplicated while referring to different paragraphs of the same article.")
+            st.info("Some references might appear to be duplicated while referring to different paragraphs of the same article.")
             for r in data_ranked:
                 doc = r.document
                 st.markdown(f'[{doc["title"]}]({doc["url"]}) [Score:{r.relevance_score:.3f}]')
